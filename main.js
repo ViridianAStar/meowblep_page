b56454129a4c2fcb368970f4353ad4101efc5b75
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-11-26 15:45:18 -0500

    Update

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ function navigateToPage(selectElement) {
         window.location.href = selectedPage + ".html";
     }
 }
+
+function checkText() {
+    document.getElementById("myTextbox").addEventListener("input", function() {
+        var textboxValue = this.value;
+        var messageElement = document.getElementById("code");
+
+        if (textboxValue == "MIIBOQIBAAJBAKoLOyblv0INCXZn9b+7+YJGtgp0sEeWS/mciyE0NGHiMikDxd/zTsZyF4hHkAYKbEhp756JKoGmU63429bvgqECAwEAAQJAGZ/fphjxHW8Bggpgiig1jWBChojXF7KaeF8SyEopqqZAhwVMIc5c66qih/higauDSibx6yTwyg0+mMdQ+ioRwQIhAN/XzJbLe1H5JBqe/VwJIjNZXgSLFUUg57req8WlN7VtAiEAwnjhQBuO71+4aosRUbFElNweKkUl0oiB0f3o/AbrpYUCIAXPTxMW4vOEuOgr+j16SzXz+tm5Vmr96KXUA7dUi3adAiBjcsFtsSoVC1QZV+iRaB90KGRcpWPEF/6bTkcYqslSqQIgXcpOTfZQ8ZXEdX64kcEasDgOKXU+MbGDeVh4BqTQATQ") {
+            navigateToPage("pages/hehe/whoami.html");
+        } else {
+            console.log("Try again later!")
+        }
+    });
+}
