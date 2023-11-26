ba62aec693b3794cce751f05f2b93045414f9af1
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-11-26 15:52:33 -0500

    A

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function checkText() {
         if (textboxValue == "MIIBOQIBAAJBAKoLOyblv0INCXZn9b+7+YJGtgp0sEeWS/mciyE0NGHiMikDxd/zTsZyF4hHkAYKbEhp756JKoGmU63429bvgqECAwEAAQJAGZ/fphjxHW8Bggpgiig1jWBChojXF7KaeF8SyEopqqZAhwVMIc5c66qih/higauDSibx6yTwyg0+mMdQ+ioRwQIhAN/XzJbLe1H5JBqe/VwJIjNZXgSLFUUg57req8WlN7VtAiEAwnjhQBuO71+4aosRUbFElNweKkUl0oiB0f3o/AbrpYUCIAXPTxMW4vOEuOgr+j16SzXz+tm5Vmr96KXUA7dUi3adAiBjcsFtsSoVC1QZV+iRaB90KGRcpWPEF/6bTkcYqslSqQIgXcpOTfZQ8ZXEdX64kcEasDgOKXU+MbGDeVh4BqTQATQ") {
             window.location.href = "pages/hehe/whoami.html";
         } else {
-            console.log("Try again later!")
+            messageElement.textContent = "";
         }
     });
 }
